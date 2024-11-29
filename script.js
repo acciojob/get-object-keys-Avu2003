0697e28bb569a0c9585222f452f3de705af955a7
Avu2003 <ajayvermaji3000@gmail.com>
2024-11-29 20:06:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+//your JS code here. If required.
+// const student={
+// 	name:"riya"
+// }
+
+// Object.prototype.getKeys=function(){
+// 	return Object.keys(this);
+// }
+// console.log(student.getkeys());
+
+const student = {
+    name: "riya"
+}
+
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+}
+
+console.log(student.getKeys());
